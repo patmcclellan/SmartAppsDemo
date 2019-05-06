0d2c7e6a09eb23b9e48b530281d5451b7c1e108c
Pat McClellan <pat@proton7group.com>
2019-05-06 15:54:25 -0700

    updates

--- force-app/main/default/lwc/levelOne/levelOne.js
+++ force-app/main/default/lwc/levelOne/levelOne.js
@@ -3,15 +3,10 @@ import { LightningElement, api, track } from 'lwc';
 export default class LevelOne extends LightningElement {
     @api recordId; 
     @api showIcon;
-    @api iconName;
     @api whatColor;
     @api whichObject;
 
     @track showLevelTwo = false;
-
-    get selectedIcon(){
-        return 'standard:' + this.iconName.toLowerCase();
-    }
 
     get colorspec(){
         return this.whatColor.toLowerCase();
