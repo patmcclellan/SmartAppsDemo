0b4b163361ea6654fa879f0e5bb3d30ac52652ce
Pat McClellan <pat@proton7group.com>
2019-05-06 16:40:10 -0700

    css test

--- force-app/main/default/lwc/levelThree/levelThree.js
+++ force-app/main/default/lwc/levelThree/levelThree.js
@@ -13,6 +13,20 @@ export default class LevelThree extends LightningElement {
     get isRecordPage(){
         return typeof this.recordId !== "undefined";
     }
-    
 
+    get talkbubble(){
+        switch(this.whatColor){
+            case "Blue":
+                return "tri-right-Blue protonLightBlue";
+            case "Yellow":
+                return "tri-right-Yello protonLightYellow";
+            case "Green":
+                return "tri-right-Green protonLightGreen";
+            case "Purple":
+                return "tri-right-Indigo protonLightIndigo";
+            default:
+                return "tri-right-White protonWhite";
+        }
+    }
+    
 }
