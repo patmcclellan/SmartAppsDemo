7648a5d6a6f0ef00a85dcbee242ec920a7d093e1
Pat McClellan <pat@proton7group.com>
2019-05-07 09:51:38 -0700

    med

--- force-app/main/default/lwc/levelThree/levelThree.js
+++ force-app/main/default/lwc/levelThree/levelThree.js
@@ -17,15 +17,15 @@ export default class LevelThree extends LightningElement {
     get talkbubble(){
         switch(this.whatColor){
             case "Blue":
-                return "talk-bubble-in round left-top shadow tri-right-Blue protonLightBlue slds-m-top_large";
+                return "talk-bubble-in round left-top shadow tri-right-Blue protonLightBlue slds-m-top_medium";
             case "Yellow":
-                return "talk-bubble-in round left-top shadow tri-right-Yellow protonLightYellow slds-m-top_large";
+                return "talk-bubble-in round left-top shadow tri-right-Yellow protonLightYellow slds-m-top_medium";
             case "Green":
-                return "talk-bubble-in round left-top shadow tri-right-Green protonLightGreen slds-m-top_large";
+                return "talk-bubble-in round left-top shadow tri-right-Green protonLightGreen slds-m-top_medium";
             case "Purple":
-                return "talk-bubble-in round left-top shadow tri-right-Indigo protonLightIndigo slds-m-top_large";
+                return "talk-bubble-in round left-top shadow tri-right-Indigo protonLightIndigo slds-m-top_medium";
             default:
-                return "talk-bubble-in round left-top shadow tri-right-White protonWhite slds-m-top_large";
+                return "talk-bubble-in round left-top shadow tri-right-White protonWhite slds-m-top_medium";
         }
     }
     
