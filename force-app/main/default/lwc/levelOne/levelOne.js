0c19ab713ede0c7c7afd32fc479df90cf47a5ec8
Pat McClellan <pat@proton7group.com>
2019-05-08 11:12:43 -0700

    no palette

--- force-app/main/default/lwc/levelOne/levelOne.js
+++ force-app/main/default/lwc/levelOne/levelOne.js
@@ -30,13 +30,14 @@ export default class LevelOne extends LightningElement {
         this.showKeycodes = !this.showKeycodes;
     }
 
-    // set up key listener
+    // SET UP A KEY LISTENER
     // docs here: https://developer.salesforce.com/docs/component-library/documentation/lwc/lwc.events_handling
+    // discussion of keydown vs keyup vs keypress can be found here:
+    // https://stackoverflow.com/questions/3396754/onkeypress-vs-onkeyup-and-onkeydown
+
     constructor() {
         super();
         this.template.addEventListener('keydown', this.shortcuts.bind(this));
-        // discussion of keydown vs keyup vs keypress can be found here:
-        // https://stackoverflow.com/questions/3396754/onkeypress-vs-onkeyup-and-onkeydown
     }
 
     // KEYBOARD SHORTCUTS
